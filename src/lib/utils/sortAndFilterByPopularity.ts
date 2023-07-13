0e88c5a84ce1c119809014962212c795b8384bb3
Narek Pogosian <narekpogosian2@gmail.com>
2023-07-13 18:10:38 +0200

    type error

--- src/lib/utils/sortAndFilterByPopularity.ts
+++ src/lib/utils/sortAndFilterByPopularity.ts
@@ -1,6 +1,9 @@
 export function sortAndFilterByPopularity<
   T extends { poster_path: string; popularity: number }
->(items: T[], minimumPopularity: number): T[] {
+>(
+  items: T[],
+  minimumPopularity: number
+): (T & { poster_path: string; popularity: number })[] {
   return items
     .filter(
       (item) => item.poster_path !== null && item.popularity > minimumPopularity
