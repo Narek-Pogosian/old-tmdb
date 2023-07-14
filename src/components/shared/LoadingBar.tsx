654a2fb52e593828252d6c039eddee9fba11d821
Narek Pogosian <narekpogosian2@gmail.com>
2023-07-14 20:50:35 +0200

    fixed, z-index on loading bar

--- src/components/shared/LoadingBar.tsx
+++ src/components/shared/LoadingBar.tsx
@@ -1,6 +1,6 @@
 const LoadingBar = () => {
   return (
-    <div className="fixed top-0 left-0 w-full h-1 bg-primary">
+    <div className="fixed top-0 left-0 z-50 w-full h-1 bg-primary">
       <div className="absolute top-0 bottom-0 w-1/3 rounded bg-tertiary progress"></div>
     </div>
   );
